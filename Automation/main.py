c2b2bceca2215f9c3f28a475609bf1231c117fad
oscaramontiel <etherealflux94@gmail.com>
2023-12-02 22:31:57 -0600

    +

--- Automation/main.py
+++ Automation/main.py
@@ -9,18 +9,21 @@ def get_driver():
   # Set options to make browsing easier
   options = webdriver.ChromeOptions()
   options.add_argument("disable-infobars")
+  options.add_argument("--headless")
+  options.add_argument("--disable-gpu")
   options.add_argument("start-maximized")
   options.add_argument("disable-dev-shm-usage")
   options.add_argument("no-sandbox")
   options.add_experimental_option("excludeSwitches", ["enable-automation"])
   options.add_argument("disable-blink-features=AutomationControlled")
+  options.add_argument("--disable-images")  # Disable images
 
   driver = webdriver.Chrome(options=options)
   driver.get("https://asalvoenlasoledad.blogspot.com/")
   return driver
 
 def get_blog_content(driver):
-  time.sleep(2)
+  time.sleep(5)
   # content = driver.find_element(By.TAG_NAME, "body").get_attribute("innerHTML")
   all_text = "\n\n".join([p.text for p in driver.find_elements(By.TAG_NAME, "p")])
   return all_text
@@ -34,18 +37,23 @@ def main():
   config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
   driver = get_driver()
   all_blog_content = ""
-  page_count = 0  # Initialize page counter
 
-  while page_count < 3:
+  #page_count = 0  # Initialize page counter
+
+  while True:
     all_blog_content += get_blog_content(driver)
+    current_url = driver.current_url  # Get the current URL
+    print("///////")
+    print(f"Scraping content from: {current_url}")  # Print or use the current URL
+    print("///////")
     try:
       next_page = driver.find_element(By.CLASS_NAME, "blog-pager-older-link")
       next_page.click()
-      time.sleep(1)
-    except Exception as e:
-      print(f"No more pages to load or error occurred: {e}")
+      time.sleep(5)
+    except Exception:
+      print("No more pages to load or error occurred.")
       break
-    page_count += 1  # Increment page counter
+    #page_count += 1  # Increment page counter
 
   driver.quit()
 
@@ -71,6 +79,7 @@ def main():
   html_content = f"<html><head>{style}</head><body style='font-family:Cardo;'>{all_blog_content}</body></html>"
 
   # Use pdfkit to generate PDF from HTML
+  print("Generating PDF...")
   pdfkit.from_string(html_content, "blog_content.pdf", configuration=config, options={'encoding': "UTF-8"})
 
 if __name__ == "__main__":
